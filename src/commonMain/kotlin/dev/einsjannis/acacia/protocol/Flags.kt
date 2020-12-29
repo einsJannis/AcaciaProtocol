bf458a7150880355c2aa6743e0fc8732cfc7c857
Roman Gräf <romangraef@loves.dicksinhisan.us>
2020-12-29 13:14:20 +0100

    flags

--- src/commonMain/kotlin/dev/einsjannis/acacia/protocol/Flags.kt
+++ src/commonMain/kotlin/dev/einsjannis/acacia/protocol/Flags.kt
@@ -1,7 +1,9 @@
 package dev.einsjannis.acacia.protocol
 
-class Flags(var value: Int) {
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
+open class Flags(var value: Int) {
     operator fun get(index: Int): Boolean = (value ushr index) and 1 == 1
 
     operator fun set(index: Int, value: Boolean) {
@@ -14,5 +16,15 @@ class Flags(var value: Int) {
     fun toggle(index: Int) {
         this.value = this.value xor (1 shl (Int.SIZE_BITS - 1 - index))
     }
+
+}
+
+class FlagBit(val index: Int) : ReadWriteProperty<Flags, Boolean> {
+    override fun setValue(thisRef: Flags, property: KProperty<*>, value: Boolean) {
+        thisRef[index] = value
+    }
+
+    override fun getValue(thisRef: Flags, property: KProperty<*>): Boolean =
+        thisRef[index]
 
 }
--- src/commonMain/kotlin/dev/einsjannis/acacia/protocol/packet/play/serverbound/ClientSettings.kt
+++ src/commonMain/kotlin/dev/einsjannis/acacia/protocol/packet/play/serverbound/ClientSettings.kt
@@ -4,6 +4,7 @@ import dev.einsjannis.acacia.protocol.Bound
 import dev.einsjannis.acacia.protocol.ConnectionState
 import dev.einsjannis.acacia.protocol.Packet
 import dev.einsjannis.acacia.protocol.PacketMeta
+import dev.einsjannis.acacia.protocol.SkinPart
 import dev.einsjannis.acacia.protocol.packet.play.ChatMode
 import dev.einsjannis.acacia.protocol.packet.play.MainHand
 
@@ -12,7 +13,7 @@ class ClientSettings : Packet() {
     var viewDistance by byte()
     var chatMode by varInt().mapped({ ChatMode.values()[it] }, { it.ordinal })
     var chatColors by bool()
-    var displayedSkinParty by ubyte() // TODO skin mapping
+    var displayedSkinParty by ubyte().mapped({ SkinPart(it.toInt()) }, { it.value.toUByte() })
     var mainHand by varInt().mapped({ MainHand.values()[it] }, { it.ordinal })
 
     companion object : PacketMeta<ClientSettings>(0x05, ConnectionState.PLAY, Bound.SERVER, ::ClientSettings)
